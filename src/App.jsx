966f70d6081417ce97b73b6557da1bcb1156103e
Desmond-Fon <desmondfon@yahoo.com>
2023-04-26 21:35:52 +0100

    added router and structure to the folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
 
 function App() {
-
-
   return (
     <>
-          <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
