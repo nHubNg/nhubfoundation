1f87a1a6946f985f8f67c44bfedff8c6c3888972
Desmond-Fon <desmondfon@yahoo.com>
2023-10-17 12:08:24 +0100

    added login screen for admin

--- src/helpers/admin/index.js
+++ src/helpers/admin/index.js
@@ -10,11 +10,11 @@ export async function signIn (email, password) {
           email: email,
           password: password,
       };
-      const result = await axios.post(`${url}/auth/sign-in`, JSON.stringify(payload),{ headers: header})
-      if (result.statusCode === 200 || result.statusCode === 201){
+      const result = await axios.post(`https://nhubfoundation-v2.onrender.com/api/v1/auth/sign-in`, JSON.stringify(payload),{ headers: header})
+      if (result.data.statusCode === 200 || result.data.statusCode === 201){
         return result
-      }else {
-        console.log(result)
+      } else {
+        return result
       }
 }
 
@@ -22,7 +22,7 @@ export async function getAllIntern() {
       const header = {
         "Content-Type": "application/json",
       };
-  const result = await axios.get(`${url}/admin/internship/get-applications`, {
+  const result = await axios.get(`https://nhubfoundation-v2.onrender.com/api/v1/admin/internship/get-applications`, {
     headers: header,
   });
    if (result.error !== null) {
