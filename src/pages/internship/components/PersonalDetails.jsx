a54b17e3c9eb14c51b572697e923c245d4a6d493
shedrachjonah11 <shedrachjonah11@gmail.com>
2023-11-07 06:50:29 +0100

    fix: validation-test

--- src/pages/internship/components/PersonalDetails.jsx
+++ src/pages/internship/components/PersonalDetails.jsx
@@ -2,11 +2,10 @@ import { useContext } from "react";
 import { FormContext } from "../../../contexts/FormContext";
 
 const PersonalDetails = () => {
-  const {formData,setFormData} = useContext(FormContext)
-
+  const { formData, setFormData } = useContext(FormContext);
 
   return (
-    <form className="grid grid-cols-1 md:grid-cols-2 gap-6 text-textAsh" >
+    <form className="grid grid-cols-1 md:grid-cols-2 gap-6 text-textAsh">
       <div className="pb-4">
         <label>
           First Name<span className="text-red">*</span>
@@ -15,7 +14,9 @@ const PersonalDetails = () => {
           type="text"
           placeholder="First Name"
           value={formData.firstname}
-          onChange={(e) => setFormData({ ...formData, firstname: e.target.value })}
+          onChange={(e) =>
+            setFormData({ ...formData, firstname: e.target.value })
+          }
           className="w-[100%] border-[#808080] border-[1px] py-2 px-2 outline-0 rounded-[5px]"
         />
       </div>
@@ -27,7 +28,9 @@ const PersonalDetails = () => {
           type="text"
           placeholder="Last Name"
           value={formData.lastname}
-          onChange={(e) => setFormData({ ...formData, lastname: e.target.value })}
+          onChange={(e) =>
+            setFormData({ ...formData, lastname: e.target.value })
+          }
           className="w-[100%] border-[#808080] border-[1px] py-2 px-2 outline-0 rounded-[5px]"
         />
       </div>
@@ -55,47 +58,44 @@ const PersonalDetails = () => {
           className="w-[100%]  border-[#808080] border-[1px] py-2 px-2 outline-0 rounded-[5px]"
         />
       </div>
-      
-        <div className="pb-4">
-          <label>
-            {" "}
-            Interview Location<span className="text-red">*</span>
-          </label>
-          <select
-            className="w-[100%] border-[#808080] border-[1px] py-2 px-2  outline-0 rounded-[5px]"
-            value={formData.interview}
-            onChange={(e) =>
-              setFormData({ ...formData, interview: e.target.value })
-            }
-          >
-            <option value="" disabled selected hidden>
-            Select 
-            </option>
-            <option value="Physical">On-Site</option>
-            <option value="Virtual">
-              Virtual (Have good network connection)
-            </option>
-          </select>
-        </div>
-        <div className="pb-4">
-          <label>
-            Gender<span className="text-red">*</span>
-          </label>
-          <select
-            className="w-[100%] border-[#808080] border-[1px] py-2 px-2 cursor-pointer outline-0 rounded-[5px]"
-            value={formData.gender}
-            onChange={(e) =>
-              setFormData({ ...formData, gender: e.target.value })
-            }
-          >
-            <option value="" disabled selected hidden>
-              Select 
-            </option>
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-          </select>
-        </div>
-      
+
+      <div className="pb-4">
+        <label>
+          {" "}
+          Interview Location<span className="text-red">*</span>
+        </label>
+        <select
+          className="w-[100%] border-[#808080] border-[1px] py-2 px-2  outline-0 rounded-[5px]"
+          value={formData.interview}
+          onChange={(e) =>
+            setFormData({ ...formData, interview: e.target.value })
+          }
+        >
+          <option value="" disabled selected hidden>
+            Select
+          </option>
+          <option value="Physical">On-Site</option>
+          <option value="Virtual">
+            Virtual (Have good network connection)
+          </option>
+        </select>
+      </div>
+      <div className="pb-4">
+        <label>
+          Gender<span className="text-red">*</span>
+        </label>
+        <select
+          className="w-[100%] border-[#808080] border-[1px] py-2 px-2 cursor-pointer outline-0 rounded-[5px]"
+          value={formData.gender}
+          onChange={(e) => setFormData({ ...formData, gender: e.target.value })}
+        >
+          <option value="" disabled selected hidden>
+            Select
+          </option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+        </select>
+      </div>
     </form>
   );
 };
--- src/pages/internship/index.jsx
+++ src/pages/internship/index.jsx
@@ -7,7 +7,6 @@ import { FormContext } from "../../contexts/FormContext";
 import { SubmitInternship } from "../../helpers/internship";
 import Modal from "../../components/Success";
 
-
 function ScrollToTopOnMount() {
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -18,10 +17,10 @@ function ScrollToTopOnMount() {
 const Internship = () => {
   const { formData, setFormData } = useContext(FormContext);
   const [page, setPage] = useState(0);
-  const [error, setError] = useState('')
-  const [loading, setLoading] = useState('')
-  const [errMsg, setErrMsg] = useState(null)
-  const [success, setSuccess] = useState(false)
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState("");
+  const [errMsg, setErrMsg] = useState(null);
+  const [success, setSuccess] = useState(false);
 
   const formTitles = [
     "Personal Details",
@@ -41,40 +40,84 @@ const Internship = () => {
 
   const pageNumber = () => {
     if (page === 0) {
-      return (<div className="text-center bg-orange rounded-[50%] text-white w-14 mx-[40%]  md:mx-[47%] p-[12px]">
-        <h1>1</h1>
-      </div>);
+      return (
+        <div className="text-center bg-orange rounded-[50%] text-white w-14 mx-[40%]  md:mx-[47%] p-[12px]">
+          <h1>1</h1>
+        </div>
+      );
     } else if (page === 1) {
-      return (<div className="text-center bg-orange rounded-[50%] text-white w-14 mx-[40%]  md:mx-[47%] p-[12px]">
-        <h1>2</h1>
-      </div>);
+      return (
+        <div className="text-center bg-orange rounded-[50%] text-white w-14 mx-[40%]  md:mx-[47%] p-[12px]">
+          <h1>2</h1>
+        </div>
+      );
     } else {
-      return (<div className="text-center bg-orange rounded-[50%] text-white w-14 mx-[40%]  md:mx-[47%] p-[12px]">
-        <h1>3</h1>
-      </div>);
+      return (
+        <div className="text-center bg-orange rounded-[50%] text-white w-14 mx-[40%]  md:mx-[47%] p-[12px]">
+          <h1>3</h1>
+        </div>
+      );
     }
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError('')
+    setError("");
     // console.log(formData);
     if (page === formTitles.length - 1) {
-      if (formData.firstname === '' || formData.lastname === '' || formData.email === '' || formData.phone === '' || formData.interview === '' || formData.gender === '' || formData.school === '' || formData.department === '' || formData.course === '' || formData.startDate === '' || formData.endDate === '' || formData.experience === '' || formData.track === '' || formData.duration === '' || formData.cover.length === 0 || formData.it.length === 0 || formData.regNo === '') {
-        return setError('Please ensure all fields are filled in before submission.')
+      if (
+        formData.firstname === "" ||
+        formData.lastname === "" ||
+        formData.email === "" ||
+        formData.phone === "" ||
+        formData.interview === "" ||
+        formData.gender === "" ||
+        formData.school === "" ||
+        formData.department === "" ||
+        formData.course === "" ||
+        formData.startDate === "" ||
+        formData.endDate === "" ||
+        formData.experience === "" ||
+        formData.track === "" ||
+        formData.duration === "" ||
+        formData.cover.length === 0 ||
+        formData.it.length === 0 ||
+        formData.regNo === ""
+      ) {
+        return setError(
+          "Please ensure all fields are filled in before submission."
+        );
       } else {
-        setError('')
-        setLoading('Loading...')
-        const res = await SubmitInternship(formData.email, formData.phone, formData.firstname, formData.lastname, formData.school, formData.department, formData.course, formData.regNo, formData.startDate, formData.endDate, formData.experience, formData.track, formData.gender, formData.interview, formData.duration, formData.it, formData.cover)
-        setLoading('')
+        setError("");
+        setLoading("Loading...");
+        const res = await SubmitInternship(
+          formData.email,
+          formData.phone,
+          formData.firstname,
+          formData.lastname,
+          formData.school,
+          formData.department,
+          formData.course,
+          formData.regNo,
+          formData.startDate,
+          formData.endDate,
+          formData.experience,
+          formData.track,
+          formData.gender,
+          formData.interview,
+          formData.duration,
+          formData.it,
+          formData.cover
+        );
+        setLoading("");
         if (res.status === 200 || res.status === 201) {
-          setLoading('')
-          alert(res.data.message)
-          setSuccess(true)
+          setLoading("");
+          alert(res.data.message);
+          setSuccess(true);
         } else {
-          console.log('error')
-        setLoading('')
-          setErrMsg(res.data.message)
+          console.log("error");
+          setLoading("");
+          setErrMsg(res.data.message);
         }
       }
     } else {
@@ -82,10 +125,47 @@ const Internship = () => {
     }
   };
 
+  const canProceed = () => {
+    if (page === 0) {
+      return (
+        formData.firstname !== "" &&
+        formData.lastname !== "" &&
+        formData.email !== "" &&
+        formData.phone !== "" &&
+        formData.interview !== "" &&
+        formData.gender !== ""
+      );
+    } else if (page === 1) {
+      return (
+        formData.school !== "" &&
+        formData.department !== "" &&
+        formData.course !== "" &&
+        formData.startDate !== "" &&
+        formData.endDate !== "" &&
+        formData.experience !== "" &&
+        formData.track !== "" &&
+        formData.duration !== ""
+      );
+    } else if (page === 2) {
+      return (
+        formData.cover.length > 0 &&
+        formData.it.length > 0 &&
+        formData.regNo !== ""
+      );
+    }
+  };
+
+  const handleNextClick = () => {
+    if (canProceed()) {
+      setPage((currPage) => currPage + 1);
+      setError("");
+    } else {
+      setError("Please ensure all fields are filled in before proceeding.");
+    }
+  };
 
   return (
     <>
-
       <ScrollToTopOnMount />
       <div className="md:flex justify-between overflow-x-hidden mb-10 w-full p-4 gap-10 px-12">
         <div className="pt-10 md:w-[40%]">
@@ -95,22 +175,17 @@ const Internship = () => {
           <div className="pt-10 text-center  font-semibold pb-8 ">
             <h1 className="text-black text-2xl">Internship Application</h1>
             <p className="text-black capitalize">All fields are Required</p>
-
           </div>
           <div className="border border-orange rounded-[10px] flex flex-col justify-center items-center">
             <div className="overflow-scroll px-10  mt-2 ">
-
               <div className="overflow-hidden">
-
                 <p className="mb-0 text-center">{pageNumber()}</p>
                 <div className="mb-4">
                   <h1 className="flex justify-center mt-6 items-center text-xl text-black">
                     {formTitles[page]}
                   </h1>
                 </div>
-                <div className="mt-2">
-                  {pageDisplay()}
-                </div>
+                <div className="mt-2">{pageDisplay()}</div>
                 <p className="text-orange text-center">{loading}</p>
                 <p className="text-btnRed text-center">{error}</p>
                 <p className="text-btnRed text-center">{errMsg}</p>
@@ -132,7 +207,7 @@ const Internship = () => {
                   <button
                     className="  py-2 px-6 mt-2 md:mt-0 md:px-9 bg-orange text-white rounded-md"
                     // disabled={!canProceed}
-                    onClick={handleSubmit}
+                    onClick={handleNextClick}
                   >
                     {page === formTitles.length - 1 ? "Apply" : " Next"}
                   </button>
@@ -141,7 +216,6 @@ const Internship = () => {
             </div>
           </div>
         </div>
-
       </div>
       {success && <Modal />}
     </>
